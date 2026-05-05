a295b7825bf7bf042835d1ddbb23222b92fe7a63
Player <player@mirror-man.local>
2026-05-05 14:48:49 +0800

    添加PWA支持（manifest + service worker + 应用图标）

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'mirror-man-v1';
+const ASSETS = ['.', 'index.html', 'icons.svg', 'favicon.svg', 'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }))
+  );
+});
